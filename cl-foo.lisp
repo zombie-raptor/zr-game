8569a7fdeeb9fc350ba3d56ff65fab716335f767
Michael Babich <michaelbabich7@gmail.com>
2013-12-29 11:45:47 -0500

    Moving the init stuff to an init function.

--- cl-foo.lisp
+++ cl-foo.lisp
@@ -6,28 +6,31 @@
 
 (defun main-loop ()
   (defun keydown-actions (scancode)
-    (format t "~a~%" (cond
-                       ((sdl2:scancode= scancode :scancode-w) "W")
-                       ((sdl2:scancode= scancode :scancode-s) "S")
-                       ((sdl2:scancode= scancode :scancode-a) "A")
-                       ((sdl2:scancode= scancode :scancode-d) "D")
-                       (t "foo"))))
+    (cond
+      ((sdl2:scancode= scancode :scancode-w) "W")
+      ((sdl2:scancode= scancode :scancode-s) "S")
+      ((sdl2:scancode= scancode :scancode-a) "A")
+      ((sdl2:scancode= scancode :scancode-d) "D")))
 
   (defun keyup-actions (scancode)
     (when (sdl2:scancode= scancode :scancode-escape)
       (sdl2:push-event :quit)))
 
+  (defun init ()
+    (gl:enable :depth-test)
+    (gl:viewport 0 0 1280 720)
+    (gl:matrix-mode :projection)
+    (gl:ortho -2 2 -2 2 -2 2)
+    (gl:matrix-mode :modelview)
+    (gl:load-identity)
+    (gl:clear-color 19/255 19/255 39/255 1.0) ; #131327 background color
+    (gl:clear :color-buffer :depth-buffer))
+
   (sdl2:with-init (:everything)
     (sdl2:with-window (win :w 1280 :h 720 :flags '(:shown :opengl))
       (sdl2:with-gl-context (gl-context win)
         (sdl2:gl-make-current win gl-context)
-        (gl:viewport 0 0 1280 720)
-        (gl:matrix-mode :projection)
-        (gl:ortho -2 2 -2 2 -2 2)
-        (gl:matrix-mode :modelview)
-        (gl:load-identity)
-        (gl:clear-color 19/255 19/255 39/255 1.0) ; #131327 background color
-        (gl:clear :color-buffer)
+        (init)
 
         (sdl2:with-event-loop (:method :poll)
           (:keydown
@@ -40,7 +43,7 @@
 
           (:idle
            ()
-           (gl:clear :color-buffer)
+           (gl:clear :color-buffer :depth-buffer)
            (gl:begin :triangles)
            (gl:color 1.0 0.0 0.0)
            (gl:vertex 0.0 1.0)
