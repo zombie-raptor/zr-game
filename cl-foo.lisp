84d92f53d11768fc42f9eb021716d4bc229dde3c
Michael Babich <michaelbabich7@gmail.com>
2013-12-29 17:14:18 -0500

    Trying and failing to enter the third dimension.

--- cl-foo.lisp
+++ cl-foo.lisp
@@ -3,12 +3,12 @@
 ;(in-package #:cl-foo)
 
 (require :sdl2)
+(require :cl-opengl)
+(require :cl-glu)
 
 (defparameter *width* 1280)
 (defparameter *height* 720)
-(defparameter *scale* 10)
-(defparameter *x* (* *width* (/ *scale* *height*)))
-(defparameter *y* *scale*)
+(defparameter *ratio* (/ *width* *height*))
 
 (defun main-loop ()
   (defun keydown-actions (scancode)
@@ -23,10 +23,12 @@
       (sdl2:push-event :quit)))
 
   (defun init ()
-    (gl:enable :depth-test)
-    (gl:viewport 0 0 *width* *height*)
+    (gl:enable :cull-face :lighting :light0 :depth-test)
     (gl:matrix-mode :projection)
-    (gl:ortho (- *x*) *x* (- *y*) *y* (- *x*) *x*)
+    (gl:load-identity)
+;    (gl:frustum -2 2 -2 2 1 10)
+    (gl:ortho 0 2 0 2 -2 10)
+;    (glu:perspective 45.0 *ratio* 0.1 10)
     (gl:matrix-mode :modelview)
     (gl:load-identity)
     (gl:clear-color 19/255 19/255 39/255 1.0) ; #131327 background color
@@ -36,6 +38,7 @@
     (sdl2:with-window (win :w *width* :h *height* :flags '(:shown :opengl))
       (sdl2:with-gl-context (gl-context win)
         (sdl2:gl-make-current win gl-context)
+        (sdl2:hide-cursor)
         (init)
 
         (sdl2:with-event-loop (:method :poll)
@@ -47,16 +50,16 @@
            (:keysym keysym)
            (keyup-actions (sdl2:scancode-value keysym)))
 
-          ;; Draws a square.
+          ;; Used to draw a square.
           (:idle
            ()
            (gl:clear :color-buffer :depth-buffer)
-           (gl:begin :quads)
-           (gl:color 1.0 0.0 0.0)
-           (gl:vertex -1.0 1.0 0.0)
-           (gl:vertex 1.0 1.0 0.0)
-           (gl:vertex 1.0 -1.0 0.0)
-           (gl:vertex -1.0 -1.0 0.0)
+           (gl:begin :quad-strip)
+           (gl:material :front :ambient-and-diffuse #(1.0 0.0 0.0 1.0))
+           (gl:vertex 0.0 1.0 2.0)
+           (gl:vertex 1.0 1.0 2.0)
+           (gl:vertex 1.0 0.0 2.0)
+           (gl:vertex 0.0 0.0 2.0)
            (gl:end)
            (gl:flush)
            (sdl2:gl-swap-window win))
