3dbb8d1d7185255b64dd334165c0eab8303830cc
Michael Babich <michaelbabich7@gmail.com>
2013-12-30 13:51:18 -0500

    Minimal working shaders.
    Added the minimum amount of shaders needed to get it to still render,
    fixed the square, changed the camera to perspective.

--- cl-foo.lisp
+++ cl-foo.lisp
@@ -1,31 +1,47 @@
 (in-package #:cl-foo)
 
 (require :sdl2)
+(require :cl-opengl)
+(require :cl-glu)
+(require :alexandria)
+
+(defun read-shader (filename)
+  (alexandria:read-file-into-string
+   (merge-pathnames (asdf:system-source-directory :cl-foo) filename)))
 
 (defun main-loop (&key (width 1280) (height 720))
   (sdl2:with-init (:everything)
-    (sdl2:with-window (win :w width :h height :flags '(:shown :opengl))
+    (sdl2:with-window (win :title "cl-foo" :w width :h height :flags '(:shown :opengl))
       (sdl2:with-gl-context (gl-context win)
         (sdl2:gl-make-current win gl-context)
         (sdl2:hide-cursor)
-        (gl:enable :cull-face :lighting :light0 :depth-test)
-        (gl:matrix-mode :projection)
-        (gl:load-identity)
-        (gl:ortho 0 2 0 2 -2 10)
-        ;; (glu:perspective 45.0 (/ width height) 0.1 10)
-        (gl:matrix-mode :modelview)
-        (gl:load-identity)
-        (gl:clear-color 19/255 19/255 39/255 1.0) ; #131327 background color
-        (gl:clear :color-buffer :depth-buffer)
 
-        ;; todo: polygon instead?
-        (gl:begin :quad-strip)
-        (gl:material :front :ambient-and-diffuse #(1.0 0.0 0.0 1.0))
-        (gl:vertex 0.0 1.0 2.0)
-        (gl:vertex 1.0 1.0 2.0)
-        (gl:vertex 1.0 0.0 2.0)
-        (gl:vertex 0.0 0.0 2.0)
-        (gl:end)
+        (gl:enable :depth-test)
+
+        (let ((vs (gl:create-shader :vertex-shader))
+              (fs (gl:create-shader :fragment-shader))
+              (program (gl:create-program)))
+
+          (gl:shader-source vs (read-shader "test.vert"))
+          (gl:shader-source fs (read-shader "test.frag"))
+          (gl:compile-shader vs)
+          (gl:compile-shader fs)
+          (format t (gl:get-shader-info-log vs))
+          (format t (gl:get-shader-info-log fs))
+          (gl:attach-shader program vs)
+          (gl:attach-shader program fs)
+          (gl:link-program program)
+          (glu:perspective 45.0 (/ width height) 0.1 100)
+          (gl:clear-color 19/255 19/255 39/255 1.0) ; #131327 background color
+          (gl:clear :color-buffer :depth-buffer)
+          (gl:use-program program))
+
+        (gl:with-primitives :quads
+          (gl:vertex -1.0 -1.0 -4.0)
+          (gl:vertex 1.0 -1.0 -4.0)
+          (gl:vertex 1.0 1.0 -4.0)
+          (gl:vertex -1.0 1.0 -4.0))
+
         (gl:flush)
         (sdl2:gl-swap-window win)
 
---
+++ test.frag
@@ -0,0 +1,6 @@
+void main()
+{
+  gl_FragColor[0] = 1.0;
+  gl_FragColor[1] = 0.0;
+  gl_FragColor[2] = 0.0;
+}
