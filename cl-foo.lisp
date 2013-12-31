9ccd6ac2e88723655eb8d3a91f5eacb1d1f236ca
Michael Babich <michaelbabich7@gmail.com>
2013-12-31 06:40:53 -0500

    Move the let statement up.

--- cl-foo.lisp
+++ cl-foo.lisp
@@ -14,14 +14,13 @@
     (sdl2:with-window (win :title "cl-foo" :w width :h height :flags '(:shown :opengl))
       (sdl2:with-gl-context (gl-context win)
         (sdl2:gl-make-current win gl-context)
-        (sdl2:hide-cursor)
-
-        (gl:enable :depth-test)
 
         (let ((vs (gl:create-shader :vertex-shader))
               (fs (gl:create-shader :fragment-shader))
               (program (gl:create-program)))
 
+          (sdl2:hide-cursor)
+          (gl:enable :depth-test)
           (gl:shader-source vs (read-shader "test.vert"))
           (gl:shader-source fs (read-shader "test.frag"))
           (gl:compile-shader vs)
@@ -34,34 +33,34 @@
           (glu:perspective 45.0 (/ width height) 0.1 100)
           (gl:clear-color 19/255 19/255 39/255 1.0) ; #131327 background color
           (gl:clear :color-buffer :depth-buffer)
-          (gl:use-program program))
+          (gl:use-program program)
 
-        (gl:with-primitives :quads
-          (gl:vertex -1.0 -1.0 -4.0)
-          (gl:vertex 1.0 -1.0 -4.0)
-          (gl:vertex 1.0 1.0 -4.0)
-          (gl:vertex -1.0 1.0 -4.0))
+          (gl:with-primitives :quads
+            (gl:vertex -1.0 -1.0 -4.0)
+            (gl:vertex 1.0 -1.0 -4.0)
+            (gl:vertex 1.0 1.0 -4.0)
+            (gl:vertex -1.0 1.0 -4.0))
 
-        (gl:flush)
-        (sdl2:gl-swap-window win)
+          (gl:flush)
+          (sdl2:gl-swap-window win)
 
-        (sdl2:with-event-loop (:method :poll)
-          (:keydown
-           (:keysym keysym)
-           (let ((scancode (sdl2:scancode-value keysym)))
-             (cond
-               ((sdl2:scancode= scancode :scancode-w) "W")
-               ((sdl2:scancode= scancode :scancode-s) "S")
-               ((sdl2:scancode= scancode :scancode-a) "A")
-               ((sdl2:scancode= scancode :scancode-d) "D"))))
+          (sdl2:with-event-loop (:method :poll)
+            (:keydown
+             (:keysym keysym)
+             (let ((scancode (sdl2:scancode-value keysym)))
+               (cond
+                 ((sdl2:scancode= scancode :scancode-w) "W")
+                 ((sdl2:scancode= scancode :scancode-s) "S")
+                 ((sdl2:scancode= scancode :scancode-a) "A")
+                 ((sdl2:scancode= scancode :scancode-d) "D"))))
 
-          (:keyup
-           (:keysym keysym)
-           (let ((scancode (sdl2:scancode-value keysym)))
-                 (when (sdl2:scancode= scancode :scancode-escape)
-                   (sdl2:push-event :quit))))
+            (:keyup
+             (:keysym keysym)
+             (let ((scancode (sdl2:scancode-value keysym)))
+               (when (sdl2:scancode= scancode :scancode-escape)
+                 (sdl2:push-event :quit))))
 
-          (:idle
-           ())
+            (:idle
+             ())
 
-          (:quit () t))))))
+            (:quit () t)))))))
