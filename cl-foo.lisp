0fb766b7350542b1885eda56dd85a980a8cf5f9f
Michael Babich <michaelbabich7@gmail.com>
2014-01-03 15:15:06 -0500

    Move shader program to a function.

--- cl-foo.lisp
+++ cl-foo.lisp
@@ -5,62 +5,64 @@
 (require :cl-glu)
 (require :alexandria)
 
-(defun read-shader (filename)
-  (alexandria:read-file-into-string
-   (merge-pathnames (asdf:system-source-directory :cl-foo) filename)))
-
-(defun use-shader (shader-type filename program)
-  (let ((shader (gl:create-shader shader-type)))
-    (gl:shader-source shader (read-shader filename))
+(defun get-shader (shader-type filename)
+  (let ((shader (gl:create-shader shader-type))
+        (current-directory (asdf:system-source-directory :cl-foo)))
+    (gl:shader-source shader
+                      (alexandria:read-file-into-string (merge-pathnames current-directory
+                                                                         filename)))
     (gl:compile-shader shader)
-    (gl:attach-shader program shader)
+    shader))
+
+(defun shader-program (shaders)
+  (let ((program (gl:create-program)))
+    (mapcar (lambda (shader) (gl:attach-shader program shader)) shaders)
     (gl:link-program program)
-    (gl:detach-shader program shader)
-    (gl:delete-shader shader)))
+    (mapcar (lambda (shader) (gl:detach-shader program shader)) shaders)
+    (mapcar #'gl:delete-shader shaders)
+    (gl:use-program program)
+    (gl:delete-program program)))
 
 (defun main-loop (&key (width 1280) (height 720))
   (sdl2:with-init (:everything)
     (sdl2:with-window (win :title "cl-foo" :w width :h height :flags '(:shown :opengl))
       (sdl2:with-gl-context (gl-context win)
         (sdl2:gl-make-current win gl-context)
+        (sdl2:hide-cursor)
+        (gl:enable :depth-test)
+        (glu:perspective 45.0 (/ width height) 0.1 100)
+        (gl:clear-color 19/255 19/255 39/255 1.0) ; #131327 background color
+        (gl:clear :color-buffer :depth-buffer)
 
-        (let ((program (gl:create-program)))
-          (sdl2:hide-cursor)
-          (gl:enable :depth-test)
-          (use-shader :vertex-shader "test.vert" program)
-          (use-shader :fragment-shader "test.frag" program)
-          (glu:perspective 45.0 (/ width height) 0.1 100)
-          (gl:clear-color 19/255 19/255 39/255 1.0) ; #131327 background color
-          (gl:clear :color-buffer :depth-buffer)
-          (gl:use-program program)
-          (gl:delete-program program)
+        (shader-program (list (get-shader :vertex-shader "test.vert")
+                              (get-shader :fragment-shader "test.frag")))
 
-          (gl:with-primitives :quads
-            (gl:vertex -1.0 -1.0 -4.0)
-            (gl:vertex 1.0 -1.0 -4.0)
-            (gl:vertex 1.0 1.0 -4.0)
-            (gl:vertex -1.0 1.0 -4.0))
+        (gl:with-primitives :quads
+          (gl:vertex -1.0 -1.0 -4.0)
+          (gl:vertex 1.0 -1.0 -4.0)
+          (gl:vertex 1.0 1.0 -4.0)
+          (gl:vertex -1.0 1.0 -4.0))
 
-          (gl:flush)
-          (sdl2:gl-swap-window win)
+        (gl:flush)
+        (sdl2:gl-swap-window win)
 
-          (sdl2:with-event-loop (:method :poll)
-            (:keydown
-             (:keysym keysym)
-             (let ((scancode (sdl2:scancode-value keysym)))
-               (cond
-                 ((sdl2:scancode= scancode :scancode-w) "W")
-                 ((sdl2:scancode= scancode :scancode-s) "S")
-                 ((sdl2:scancode= scancode :scancode-a) "A")
-                 ((sdl2:scancode= scancode :scancode-d) "D"))))
+        (sdl2:with-event-loop (:method :poll)
+          (:keydown
+           (:keysym keysym)
+           (let ((scancode (sdl2:scancode-value keysym)))
+             (cond
+               ((sdl2:scancode= scancode :scancode-w) "W")
+               ((sdl2:scancode= scancode :scancode-s) "S")
+               ((sdl2:scancode= scancode :scancode-a) "A")
+               ((sdl2:scancode= scancode :scancode-d) "D"))))
 
-            (:keyup
-             (:keysym keysym)
-             (let ((scancode (sdl2:scancode-value keysym)))
-               (when (sdl2:scancode= scancode :scancode-escape)
-                 (sdl2:push-event :quit))))
+          (:keyup
+           (:keysym keysym)
+           (let ((scancode (sdl2:scancode-value keysym)))
+             (when (sdl2:scancode= scancode :scancode-escape)
+               (sdl2:push-event :quit))))
 
-            (:idle
-             ())
+          (:idle
+           ())
 
-            (:quit () t)))))))
+          (:quit () t))))))
