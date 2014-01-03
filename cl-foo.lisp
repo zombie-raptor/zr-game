1d506a37b42cb20a10bf7d8ba245552f66fe425c
Michael Babich <michaelbabich7@gmail.com>
2014-01-03 14:09:31 -0500

    Move shader logic to a function.

--- cl-foo.lisp
+++ cl-foo.lisp
@@ -9,31 +9,31 @@
   (alexandria:read-file-into-string
    (merge-pathnames (asdf:system-source-directory :cl-foo) filename)))
 
+(defun use-shader (shader-type filename program)
+  (let ((shader (gl:create-shader shader-type)))
+    (gl:shader-source shader (read-shader filename))
+    (gl:compile-shader shader)
+    (gl:attach-shader program shader)
+    (gl:link-program program)
+    (gl:detach-shader program shader)
+    (gl:delete-shader shader)))
+
 (defun main-loop (&key (width 1280) (height 720))
   (sdl2:with-init (:everything)
     (sdl2:with-window (win :title "cl-foo" :w width :h height :flags '(:shown :opengl))
       (sdl2:with-gl-context (gl-context win)
         (sdl2:gl-make-current win gl-context)
 
-        (let ((vert (gl:create-shader :vertex-shader))
-              (frag (gl:create-shader :fragment-shader))
-              (program (gl:create-program)))
-
+        (let ((program (gl:create-program)))
           (sdl2:hide-cursor)
           (gl:enable :depth-test)
-          (gl:shader-source vert (read-shader "test.vert"))
-          (gl:shader-source frag (read-shader "test.frag"))
-          (gl:compile-shader vert)
-          (gl:compile-shader frag)
-          (format t (gl:get-shader-info-log vert))
-          (format t (gl:get-shader-info-log frag))
-          (gl:attach-shader program vert)
-          (gl:attach-shader program frag)
-          (gl:link-program program)
+          (use-shader :vertex-shader "test.vert" program)
+          (use-shader :fragment-shader "test.frag" program)
           (glu:perspective 45.0 (/ width height) 0.1 100)
           (gl:clear-color 19/255 19/255 39/255 1.0) ; #131327 background color
           (gl:clear :color-buffer :depth-buffer)
           (gl:use-program program)
+          (gl:delete-program program)
 
           (gl:with-primitives :quads
             (gl:vertex -1.0 -1.0 -4.0)
@@ -63,9 +63,4 @@
             (:idle
              ())
 
-            (:quit
-             ()
-             (gl:delete-shader vert)
-             (gl:delete-shader frag)
-             (gl:delete-program program)
-             t)))))))
+            (:quit () t)))))))
