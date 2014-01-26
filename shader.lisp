7422bfc7c3686a186f0a6f3bdc654171f55573c5
Michael Babich <michaelbabich7@gmail.com>
2014-01-25 23:21:06 -0500

    Move shaders back into Lisp strings.

--- shader.lisp
+++ shader.lisp
@@ -4,28 +4,29 @@
 
 (in-package #:cl-foo)
 
-(defun glsl-action (l)
-  (let ((symbol (elt l 0))
-        (first (nth 1 l))
-        (rest (nthcdr 2 l)))
-    (case symbol
-      ((+) (format nil "(~S~{ + ~S~})" first rest))
-      ((-) (if (eq (length l) 2)
-               (format nil "-(~S)" first)
-               (format nil "(~S~{ - ~S~})" first rest)))
-      ((*) (format nil "(~S~{ * ~S~})" first rest))
-      ((/) (format nil "(~S~{ / ~S~})" first rest))
-      ((>) (format nil "(~S~{ > ~S~})" first rest))
-      ((<) (format nil "(~S~{ < ~S~})" first rest))
-      ((>=) (format nil "(~S~{ >= ~S~})" first rest))
-      ((<=) (format nil "(~S~{ <= ~S~})" first rest))
-      ((not) (format nil "!(~S)" first))
-      ((and) (format nil "(~S~{ && ~S~})" first rest))
-      ((or) (format nil "(~S~{ || ~S~})" first rest))
-      ((in) (format nil "layout(location = ~S) in ~A ~A;" (nth 1 l) (nth 2 l) (nth 3 l)))
-      ((out) (format nil "layout(location = ~S) out ~A ~A;" (nth 1 l) (nth 2 l) (nth 3 l)))
-      ((uniform) (format nil "uniform ~A ~A;" (nth 1 l) (nth 2 l)))
-      (otherwise (error (format nil "Not supported operator. ~S" symbol))))))
+(defun make-glsl-operator (symbol first rest)
+  (case symbol
+    ((+) (format nil "(~S~{ + ~S~})" first rest))
+    ((-) (if (eq (length rest) 0)
+             (format nil "-(~S)" first)
+             (format nil "(~S~{ - ~S~})" first rest)))
+    ((*) (format nil "(~S~{ * ~S~})" first rest))
+    ((/) (format nil "(~S~{ / ~S~})" first rest))
+    ((>) (format nil "(~S~{ > ~S~})" first rest))
+    ((<) (format nil "(~S~{ < ~S~})" first rest))
+    ((>=) (format nil "(~S~{ >= ~S~})" first rest))
+    ((<=) (format nil "(~S~{ <= ~S~})" first rest))
+    ((not) (format nil "!(~S)" first))
+    ((and) (format nil "(~S~{ && ~S~})" first rest))
+    ((or) (format nil "(~S~{ || ~S~})" first rest))))
+
+(defun make-glsl-line (l)
+  (let ((symbol (nth 0 l)))
+    (ccase symbol
+      ((+ - * / > < >= <= not and or) (make-glsl-operator symbol (nth 1 l) (nthcdr 2 l)))
+      ((in) (format nil "layout(location = ~D) in ~A ~A;~%" (nth 1 l) (nth 2 l) (nth 3 l)))
+      ((out) (format nil "layout(location = ~D) out ~A ~A;~%" (nth 1 l) (nth 2 l) (nth 3 l)))
+      ((uniform) (format nil "uniform ~A ~A;~%" (nth 1 l) (nth 2 l))))))
 
 (defun read-shader (shader-string shader-type)
   (let ((shader (gl:create-shader shader-type)))
@@ -44,10 +45,10 @@
     (map nil #'(lambda (shader) (gl:detach-shader program shader)) shaders)
     program))
 
-(defmacro with-shaders ((shaders program &key shader-list) &body body)
+(defmacro with-shaders ((shaders program &key shader-list shader-type-list) &body body)
   ;; FIXME: Assumes that all of the given shaders are used in the same
   ;; program.
-  `(let* ((,shaders (map nil #'read-shader ,shader-list))
+  `(let* ((,shaders (mapcar #'read-shader ,shader-list ,shader-type-list))
           (,program (shader-program ,shaders)))
      (unwind-protect
           (progn ,@body)
--- test.frag
+++
@@ -1,8 +0,0 @@
-#version 330
-
-out vec4 out_color;
-
-void main(void)
-{
-  out_color = mix(vec4(1.0f, 0.5f, 0.5f, 1.0f), vec4(0.5f, 0.5f, 1.0f, 1.0f), (gl_FragCoord.y) / 720.0f);
-}
